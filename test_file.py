a6b59e508354d28465c2aa13a591123d9107d0d2
ADARI GIRISH KUMAR <adari.girishkumar@gmail.com>
2020-03-04 23:11:44 +0530

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -6,7 +6,7 @@ password_input = '//*[@id="pass"]'
 login_submit = '//*[@id="u_0_b"]'
 with open('passwordfile.txt', 'r') as myfile:
     password = myfile.read().replace('\n', '')
-driver.find_element_by_xpath(username_input).send_keys("7550249736")
+driver.find_element_by_xpath(username_input).send_keys("username")
 driver.find_element_by_xpath(password_input).send_keys(password)
 driver.find_element_by_xpath(login_submit).click()
 
